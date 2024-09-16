429a3768a02d06cd257b6697cd4f70fad70b1135
Matthew2357 <71255969+Matthew2357@users.noreply.github.com>
2024-09-16 16:56:24 +0000

    small fixes

--- src/hetlora/distribute.py
+++ src/hetlora/distribute.py
@@ -5,4 +5,5 @@ from models.lora import GPTLoRA
 from .clients import Client
 
 def distribute(global_model: GPTLoRA, hetlora_ranks: list[int], opt, scheduler):
-    return [[global_model.truncate(rank), opt, scheduler] for rank in hetlora_ranks]
+    max_rank = max(hetlora_ranks)
+    return [[global_model.truncate(rank, max_rank), opt, scheduler] for rank in hetlora_ranks]
